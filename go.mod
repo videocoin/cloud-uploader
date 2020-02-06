e2ded2e52700af873fea63370e997fcda3d38047
cyc60 <avsysoev60@gmail.com>
2020-02-06 21:13:36 +0300

    Deps

--- go.mod
+++ go.mod
@@ -5,15 +5,18 @@ go 1.12
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/dwbuiten/go-mediainfo v0.0.0-20150630175133-91f51f40c56a // indirect
+	github.com/gogo/protobuf v1.3.1
 	github.com/google/uuid v1.0.0
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/nareix/joy4 v0.0.0-20181022032202-3ddbc8f9d431 // indirect
 	github.com/opentracing/opentracing-go v1.1.0
 	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.4.0
 	github.com/videocoin/cloud-api v0.0.17
 	github.com/videocoin/cloud-pkg v0.0.6
 	google.golang.org/api v0.14.0
+	google.golang.org/grpc v1.21.1
 	gopkg.in/redis.v5 v5.2.9
 )
 
---
+++ vendor/github.com/videocoin/cloud-pkg/auth/auth.go
@@ -0,0 +1,50 @@
+package auth
+
+import (
+	"context"
+	"errors"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	grpcauth "github.com/grpc-ecosystem/go-grpc-middleware/auth"
+)
+
+var (
+	ErrInvalidSecret = errors.New("invalid secret")
+	ErrInvalidToken  = errors.New("invalid token")
+)
+
+type TokenType int32
+
+type ExtendedClaims struct {
+	Type TokenType `json:"token_type"`
+	jwt.StandardClaims
+}
+
+func AuthFromContext(ctx context.Context) (context.Context, string, error) {
+	secret, ok := SecretKeyFromContext(ctx)
+	if !ok {
+		return ctx, "", ErrInvalidSecret
+	}
+
+	jwtToken, err := grpcauth.AuthFromMD(ctx, "bearer")
+	if err != nil {
+		return ctx, "", ErrInvalidToken
+	}
+
+	t, err := jwt.ParseWithClaims(jwtToken, &ExtendedClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return ctx, "", err
+	}
+
+	if !t.Valid {
+		return ctx, "", ErrInvalidToken
+	}
+
+	ctx = NewContextWithUserID(ctx, t.Claims.(*ExtendedClaims).Subject)
+	ctx = NewContextWithType(ctx, t.Claims.(*ExtendedClaims).Type)
+
+	return ctx, jwtToken, nil
+}
